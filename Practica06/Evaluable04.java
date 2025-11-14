6b2c9d421ae001c60e031711d7e5955274fb8948
LuiEnMG <mattagonzalez.luissec10@gmail.com>
2025-11-14 11:02:40 -0600

    bub

--- Practica06/Evaluable04.java
+++
@@ -1,6 +0,0 @@
-package practica06;
-
-public interface Evaluable04 {
-    // registra y retorna nivel de evaluacion (0.0 a 1.0)
-    double evaluar(double score);
-}
--- Practica06/Promovible4428.java
+++
@@ -1,6 +0,0 @@
-package practica06;
-
-public interface Promovible4428 {
-    boolean esPromovible();
-    void promover();  // aplica cambio de nivel/sueldo
-}
